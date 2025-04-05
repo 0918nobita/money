b83837ee4b41d26202e3b53143eda56eab22c829
Kodai Matsumoto <nobita.0918@gmail.com>
2025-04-05 17:40:26 +0900

    資産推移を highcharts の折れ線グラフで可視化した

--- frontend/gas.ts
+++
@@ -1,9 +0,0 @@
-export const getData = () =>
-	new Promise<{ foo: number }>((resolve) => {
-		google.script.run
-			.withSuccessHandler((res) => {
-				console.log(res);
-				resolve(res);
-			})
-			.getData();
-	});
--- keep.ts
+++
@@ -1,7 +0,0 @@
-export const keepFunctions = (
-	// biome-ignore lint/suspicious/noExplicitAny:
-	...funcs: Array<(...args: any[]) => any>
-) => {
-	for (const _func of funcs) {
-	}
-};
