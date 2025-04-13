f8c8a13c611c1b85151878ad10709639d7e01ccc
Kodai Matsumoto <nobita.0918@gmail.com>
2025-04-13 23:04:02 +0900

    円グラフを描画するサンプルを追加した

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -10,6 +10,10 @@ const LineChart = lazy(() =>
     import('@mui/x-charts/LineChart').then((m) => ({ default: m.LineChart })),
 );
 
+const PieChart = lazy(() =>
+    import('@mui/x-charts/PieChart').then((m) => ({ default: m.PieChart })),
+);
+
 function RouteComponent() {
     const { currentBalance, balanceHistory } = Route.useLoaderData();
 
@@ -47,9 +51,23 @@ function RouteComponent() {
                 ]}
                 width={600}
                 height={400}
-                grid={{ horizontal: true, vertical: true }}
+                grid={{ horizontal: true }}
                 viewBox={{ x: -50, y: 50, width: 650, height: 300 }}
                 title="資産推移"
+            />
+
+            <PieChart
+                series={[
+                    {
+                        data: [
+                            { id: 0, value: 10, label: 'foo' },
+                            { id: 1, value: 20, label: 'bar' },
+                        ],
+                    },
+                ]}
+                width={400}
+                height={200}
+                title="支出内訳"
             />
 
             <p>現在の総資産は {formatter.format(currentBalance)} 円です</p>
